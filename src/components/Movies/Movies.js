410bd5d088b274cb6b240509314a57e52958df2d
Vasya <vasilgor@rambler.ru>
2022-08-21 02:56:47 +0300

    1try

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,13 +1,10 @@
-import { useState, useEffect, useContext } from 'react';
+import { useState, useEffect, useContext } from "react";
 import SearchForm from "./SearchForm/SearchForm";
 import MoviesCardList from "./MoviesCardList/MoviesCardList";
 import Footer from "./../Footer/Footer";
-import { CurrentUserContext } from '../../context/CurrentUserContext';
-import { useLocation } from 'react-router-dom';
-export default function Movies({ loading }) 
-
-
-{
+import { CurrentUserContext } from "../../context/CurrentUserContext";
+import { useLocation } from "react-router-dom";
+export default function Movies({ loading }) {
   const {
     cards,
     handlerSearchForm,
@@ -22,7 +19,7 @@ export default function Movies({ loading })
     setSearchForm,
     handleCardLike,
     savedCards,
-    getSavedCard 
+    getSavedCard,
   } = useContext(CurrentUserContext);
 
   const location = useLocation();
@@ -35,10 +32,8 @@ export default function Movies({ loading })
   const [movies, setMovies] = useState([]);
   const [renderedMovies, setRenderedMovies] = useState([]);
   const [idx, setIdx] = useState(0);
-  const [countMoviesWindow, setCountMoviesWindow] = useState(0)
+  const [countMoviesWindow, setCountMoviesWindow] = useState(0);
   const [winWidth, setWinWidth] = useState(window.innerWidth);
-
-
 
   function updateWindowWidth() {
     setWinWidth(window.innerWidth);
@@ -48,9 +43,12 @@ export default function Movies({ loading })
     const searchRequestData = JSON.parse(localStorage.searchRequest);
     const checkBoxState = searchRequestData.checkBox;
     const request = searchRequestData.request;
-    return unmovies.filter(movie => {
+    return unmovies.filter((movie) => {
       if (checkBoxState === true) {
-        return movie.duration <= 40 && movie.nameRU.toLowerCase().includes(request.toLowerCase());
+        return (
+          movie.duration <= 40 &&
+          movie.nameRU.toLowerCase().includes(request.toLowerCase())
+        );
       } else {
         return movie.nameRU.toLowerCase().includes(request.toLowerCase());
       }
@@ -58,7 +56,7 @@ export default function Movies({ loading })
   }
 
   function getMoviesFromLocalStorage() {
-    if (localStorage.getItem('searchRequest') !== null) {
+    if (localStorage.getItem("searchRequest") !== null) {
       const searchRequestData = JSON.parse(localStorage.searchRequest);
       const movies = filterMovies(searchRequestData.movies);
       setMovies(movies);
@@ -76,31 +74,31 @@ export default function Movies({ loading })
 
   function renderMovies() {
     if (winWidth > 1154) {
-      setIdx(prev => prev + 3)
+      setIdx((prev) => prev + 3);
+    } else if (winWidth < 768) {
+      setIdx((prev) => prev + 2);
     }
-    else if (winWidth < 768) {
-      setIdx(prev => prev + 2)
-    }
-    const moviesToRender = movies.slice(renderedMovies.length, countMoviesWindow + idx);
+    const moviesToRender = movies.slice(
+      renderedMovies.length,
+      countMoviesWindow + idx
+    );
     setRenderedMovies([...renderedMovies, ...moviesToRender]);
-
   }
   useEffect(() => {
     if (winWidth > 1150) {
-      setCountMoviesWindow(12)
+      setCountMoviesWindow(12);
     }
     if (winWidth < 1150) {
-      setCountMoviesWindow(8)
+      setCountMoviesWindow(8);
     }
     if (winWidth < 767) {
-      setCountMoviesWindow(5)
+      setCountMoviesWindow(5);
     }
-
-  }, [winWidth])
+  }, [winWidth]);
 
   useEffect(() => {
-    renderMovies()
-  }, [movies]); 
+    renderMovies();
+  }, [movies]);
 
   useEffect(() => {
     getMoviesFromLocalStorage();
@@ -108,42 +106,49 @@ export default function Movies({ loading })
     return () => {
       setMovies([]);
       setRenderedMovies([]);
-    }
-  }, []); 
+    };
+  }, []);
 
-
-
-return (
+  return (
     <section className="movies">
       <SearchForm
-            setRenderedMovies={setRenderedMovies}
-            setSearchForm={setSearchForm}
-            handleSearchSubmit={handleSearchSubmit}
-            setSearchInput={setSearchInput}
-            cards={cards}
-            handlerSearchForm={handlerSearchForm}
-            searchForm={searchForm}
-            searchFormDirty={searchFormDirty}
-            searchFormError={searchFormError}
-            setFormValid={setFormValid}
-            formValid={formValid}
-            setCards={setCards}
-            handleSearchCard={handleSearchCard}
+        setRenderedMovies={setRenderedMovies}
+        setSearchForm={setSearchForm}
+        handleSearchSubmit={handleSearchSubmit}
+        setSearchInput={setSearchInput}
+        cards={cards}
+        handlerSearchForm={handlerSearchForm}
+        searchForm={searchForm}
+        searchFormDirty={searchFormDirty}
+        searchFormError={searchFormError}
+        setFormValid={setFormValid}
+        formValid={formValid}
+        setCards={setCards}
+        handleSearchCard={handleSearchCard}
       />
-   
-        <MoviesCardList
-         getSavedCard={getSavedCard} savedCards={savedCards} handleCardLike={handleCardLike} cards={renderedMovies} loading={loading}
-        />
-     
-     {Boolean(location.pathname === '/movies' & (filterMovies.length > renderMovies.length)) &&
-          <button type="button"
-            className="movies__next-button"
-            onClick={renderMovies}
-          >Ещё</button>
-        }
 
-<Footer />
+      <MoviesCardList
+        getSavedCard={getSavedCard}
+        savedCards={savedCards}
+        handleCardLike={handleCardLike}
+        cards={renderedMovies}
+        loading={loading}
+      />
+
+      {Boolean(
+        (location.pathname === "/movies") &
+          (filterMovies.length > renderMovies.length)
+      ) && (
+        <button
+          type="button"
+          className="movies__next-button"
+          onClick={renderMovies}
+        >
+          Ещё
+        </button>
+      )}
+
+      <Footer />
     </section>
   );
-
 }
